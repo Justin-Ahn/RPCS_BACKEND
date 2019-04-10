2a1ee441910d7a8d8dd4fd7bca43973147922d70
justin-ahn <justinahn0@gmail.com>
2019-04-10 17:39:39 -0400

    whoops

--- rpcs_db_server/rpcs_db_server/views.py
+++ rpcs_db_server/rpcs_db_server/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
+from rpcs_db_server.utils import is_authorized
+import base64
 
 
 def health(request):
@@ -11,6 +13,9 @@ def health(request):
 def auth(request): 
     if request.method != "GET":        
         raise Http404("HTTP Verbs Accepted: GET")	
-    # Need to check Auth header, decode, and describe permissions. 
+
+    if not is_authorized(request):
+        return HttpResponse('Unauthorized', status=401)
+        
     return JsonResponse({'foo':'auth'})
 
