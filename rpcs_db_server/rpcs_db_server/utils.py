ba6c5e524611ff14a0913def5de6837701540ac5
justin-ahn <shinramen12>
2019-04-12 17:41:31 -0400

    A large structural change... -> 1) Better error handling (handling malformed queries & data pushes) 2) More descriptive error/success messages 3) Now ingests all elements of json array (or errors out) 4) WIP in attempting to reduce all duplicate code/functionality into rpcs_db_server.utils

--- rpcs_db_server/rpcs_db_server/utils.py
+++ rpcs_db_server/rpcs_db_server/utils.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.core import serializers
+from django.http import HttpResponse
+from django.forms.models import modelform_factory
+import json
 import base64
 
 def authorized(request, action):
@@ -19,3 +23,50 @@ def authorized(request, action):
     else:
         return False
 
+
+def ingest_data(request, model):
+    payload = json.loads(request.body.decode())
+    received_data = []
+    form = modelform_factory(model, fields=('patient_name', 'patient_id', 'scaled_rating1', 'scaled_rating2',
+                                              'test_results'))
+
+    for json_entry in payload:
+        populated_form = form(data=json_entry)
+        if populated_form.is_valid():
+            received_data.append(populated_form)
+        else:
+            return HttpResponse('RPCS Backend Server: Data push denied -- invalid params.', status=400)
+
+    for data_entry in received_data:
+        data_entry.save()
+
+    response_text = 'RPCS Backend Server: Accepted ' + \
+                    str(len(received_data)) + (' entry' if len(received_data) == 1 else ' entries')
+    return HttpResponse(response_text, status=200)
+
+
+def return_data(request, model, param):
+    target = request.GET.get(param, '')
+    try:
+        pid_to_get = int(target)
+    except ValueError:
+        if target == '':
+            pid_to_get = None
+        else:
+            return HttpResponse('RPCS Backend Server: Data pull denied -- invalid params.', status=400)
+
+    all_data = serializers.serialize('json', model.objects.all())
+    data_list = json.loads(all_data)
+    mapped_list = list(map(lambda x: x['fields'], data_list)) # 'fields' -> a relic of how django processes data
+
+    if pid_to_get is not None:
+        response = list(filter(lambda x: x[param] == pid_to_get, mapped_list))
+    else:
+        response = mapped_list
+
+    return HttpResponse(json.dumps(response), content_type='application/json', status=200)
+
+
+def handle_invalid_request(request):
+    response_text = 'RPCS Backend Server: Invalid HTTP request method'
+    return HttpResponse(response_text, status=405)
