7d9307af1d8d71b680e63d8ce48256d92f510f10
justin-ahn <shinramen12>
2019-04-12 18:09:54 -0400

    Forgot to generalize fields

--- rpcs_db_server/stm/views.py
+++ rpcs_db_server/stm/views.py
@@ -13,10 +13,11 @@ def tests(request):
     if not authorized(request, None):
         return HttpResponse('Unauthorized', status=401)
 
+    my_fields = ('patient_name', 'patient_id', 'scaled_rating1', 'scaled_rating2', 'test_results')
     if request.method == "GET":
         return return_data(request, Results, 'patient_id')
     elif request.method == "POST":
-        return ingest_data(request, Results)
+        return ingest_data(request, Results, my_fields)
     else:
         return handle_invalid_request(request)
 
