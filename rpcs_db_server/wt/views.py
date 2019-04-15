be6394baf3ad2fdd6a8bdfd6d1713382adbcec67
justin-ahn <shinramen12>
2019-04-15 00:34:10 -0400

    Generalization of group-specific json customizations

--- rpcs_db_server/wt/views.py
+++ rpcs_db_server/wt/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
-from rpcs_db_server.utils import authorized, ingest_data, return_data, handle_invalid_request
+from rpcs_db_server.utils import authorized, ingest_data, return_data, handle_invalid_request, json_timestamp_customizer
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
 from wt.models import Patient, Caregiver, Safezone
@@ -31,7 +31,7 @@ def caregiver(request):
     if request.method == "GET":
         return return_data(request, Caregiver, 'patient_id')
     elif request.method == "POST":
-        return ingest_data(request, Caregiver, my_fields)
+        return ingest_data(request, Caregiver, my_fields, json_customizer=json_timestamp_customizer)
     else:
         return handle_invalid_request(request)
 
