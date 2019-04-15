aa19da93806b168ffb20564f623b36f437c2336a
justin-ahn <shinramen12>
2019-04-15 11:46:02 -0400

    include superuser in authorizations

--- rpcs_db_server/rpcs_db_server/utils.py
+++ rpcs_db_server/rpcs_db_server/utils.py
@@ -21,11 +21,10 @@ def authorized(request, action):
     # A really bad hack for permissions but hopefully this will suffice. Permissions currently depend on parsing
     # username & seeing that it corresponds with the api endpoint the user is hitting
     if user is not None:
-        if request.method == "GET" and "readonly" in username:
-            return True
-        return action in username
-    else:
-        return False
+        return user.is_superuser or \
+               request.method == "GET" and "readonly" in username or \
+               action in username
+    return False
 
 
 def json_timestamp_customizer(json_data):
