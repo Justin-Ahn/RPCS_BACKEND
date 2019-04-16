1158875fe1967c740e367e541428d5aa8df95310
justin-ahn <justinahn0@gmail.com>
2019-04-16 17:47:57 -0400

    ca: refactor ca.py

--- rpcs_db_server/ca.py
+++ rpcs_db_server/ca.py
@@ -16,61 +16,63 @@ def main():
     except (Exception, psycopg2.Error) as error:
         print('Error while fetching data from postgreSQL', error)
     else:
-        # home sensor: determine room entry
-        hs_br_flag = False
-        hs_br_rfid = False
-        hs_br_motn = False
-        hs_br_entry_ts = datetime.datetime(2005, 7, 14, 12, 30, tzinfo=utc)
-        
-
-        hs_kt_flag = False
-        hs_kt_rfid = False
-        hs_kt_motn = False
-        hs_kt_entry_ts = datetime.datetime(2005, 7, 14, 12, 30)
-        postgreSQL_select_Query = "select * from hs_events where " + filter_night
-        
-        cursor.execute(postgreSQL_select_Query)
-        print('Selecting rows from hs table using cursor.fetchall')
-        hs_events = cursor.fetchall()
-        for row in hs_events:
-            if not hs_br_flag:
-                if row[3] == 'rfid' and 'BROKEN' in row[5]:
-                    if hs_br_motn and (row[4] - hs_br_entry_ts).total_seconds() < 200:
-                        hs_br_flag = True
-                        # if at night, add bathroom entry into sleep_trend table
-                        if check_night(hs_br_entry_ts):
-                            update_night_br_usage(connection, cursor, hs_br_entry_ts)
-                        print("New entry at: ", hs_br_entry_ts)
-                    else:
-                        hs_br_entry_ts = row[4]
-                        hs_br_rfid = True
-                if row[3] == 'motion' and 'MOTION_STARTED' in row[5]:
-                    if hs_br_rfid and (row[4] - hs_br_entey_ts).total_seconds() < 200:
-                        hs_br_flag = True
-                        # if at night, add bathroom entry into sleep_trend table
-                        if check_night(hs_br_entry_ts):
-                            update_night_br_usage(connection, cursor, hs_br_entry_ts)
-                        print("New entry at: ", hs_br_entry_ts)
-                    else:
-                        hs_br_entry_ts = row[4]
-                        hs_br_motn = True
-            else:
-                if row[3] == 'rfid' and 'UNBROKEN' not in row[5]:
-                    # insert bathroom exit into table
-                    print("New exit at: ", row[4])
-                        
-            print('ID = ', row[0])
-            print('event_type = ', row[1])
-            print('sensor_id = ', row[2])
-            print('sensor_type = ', row[3])
-            print('timestamp = ', row[4])
-            print('data = ', row[5])
-            print('event_id = ', row[6], '\n')
+        hs_analysis(connection, cursor)
     finally:
         if connection:
             cursor.close()
             connection.close()
             print('PostgreSQL connection is closed')
+
+def hs_analysis(connection, cursor):
+    # home sensor: determine room entry
+    hs_br_flag = False
+    hs_br_rfid = False
+    hs_br_motn = False
+    hs_br_entry_ts = datetime.datetime(2005, 7, 14, 12, 30, tzinfo=utc)    
+
+    hs_kt_flag = False
+    hs_kt_rfid = False
+    hs_kt_motn = False
+    hs_kt_entry_ts = datetime.datetime(2005, 7, 14, 12, 30)
+    postgreSQL_select_Query = "select * from hs_events where " + filter_night
+        
+    cursor.execute(postgreSQL_select_Query)
+    print('Selecting rows from hs table using cursor.fetchall')
+    hs_events = cursor.fetchall()
+    for row in hs_events:
+        if not hs_br_flag:
+            if row[3] == 'rfid' and 'BROKEN' in row[5]:
+                if hs_br_motn and (row[4] - hs_br_entry_ts).total_seconds() < 200:
+                    hs_br_flag = True
+                    # if at night, add bathroom entry into sleep_trend table
+                    if check_night(hs_br_entry_ts):
+                        update_night_br_usage(connection, cursor, hs_br_entry_ts)
+                    print("New entry at: ", hs_br_entry_ts)
+                else:
+                    hs_br_entry_ts = row[4]
+                    hs_br_rfid = True
+            if row[3] == 'motion' and 'MOTION_STARTED' in row[5]:
+                if hs_br_rfid and (row[4] - hs_br_entey_ts).total_seconds() < 200:
+                    hs_br_flag = True
+                    # if at night, add bathroom entry into sleep_trend table
+                    if check_night(hs_br_entry_ts):
+                        update_night_br_usage(connection, cursor, hs_br_entry_ts)
+                    print("New entry at: ", hs_br_entry_ts)
+                else:
+                    hs_br_entry_ts = row[4]
+                    hs_br_motn = True
+        else:
+            if row[3] == 'rfid' and 'UNBROKEN' not in row[5]:
+                # insert bathroom exit into table
+                print("New exit at: ", row[4])
+                        
+        print('ID = ', row[0])
+        print('event_type = ', row[1])
+        print('sensor_id = ', row[2])
+        print('sensor_type = ', row[3])
+        print('timestamp = ', row[4])
+        print('data = ', row[5])
+        print('event_id = ', row[6], '\n')
 
 def update_night_br_usage(connection, cursor, timestamp):
     select_query = "select * from ca_sleep_trend where date = %s"
@@ -86,7 +88,7 @@ def update_night_br_usage(connection, cursor, timestamp):
         update_query = "update ca_sleep_trend set num_go_to_bathroom = num_go_to_bathroom + 1 where patient_id = 1 and date = %s"
         cursor.execute(update_query, (curdate,))
     connection.commit()
-    print('Added one sleep trend record: ', cursor.fetchone())
+    print('Successfully update night bathroom usage')
 
 def check_night(timestamp):
     return night_start <= timestamp.time() <= night_end
