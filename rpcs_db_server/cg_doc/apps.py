59bea2007fc6a3b9bfb89d3031355f0358fb8c7c
justin-ahn <justinahn0@gmail.com>
2019-04-24 23:41:02 -0400

    How did that get here... Removing relic of old times

--- rpcs_db_server/cg_doc/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CgDocConfig(AppConfig):
-    name = 'cg_doc'
--- rpcs_db_server/cg_doc/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from cg_doc import views
-
-urlpatterns = [
-    path('caregiver-profile', views.caregiver_profile),
-    path('doctor-profile', views.doctor_profile)
-]
