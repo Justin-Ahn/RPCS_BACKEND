b91c7f423db0e5e63da424a88fd1c41a3aa3dd9d
justin-ahn <justinahn0@gmail.com>
2019-04-26 23:23:37 -0400

    ca: try to send real time alert but failed

--- rpcs_db_server/hs/views.py
+++ rpcs_db_server/hs/views.py
@@ -3,7 +3,8 @@ from django.views.decorators.csrf import csrf_exempt
 from hs.models import Events, Sensors
 from rpcs_db_server.utils import authorized, ingest_data, return_data, handle_invalid_request, \
     json_timestamp_customizer, json_j2str_customizer
-
+import psycopg2
+import requests
 
 # Create your views here.
 
@@ -36,11 +37,31 @@ def sensors(request):
     if request.method == "GET":
         return return_data(request, Sensors, filterable_params)
     elif request.method == "POST":
-        return ingest_data(request, Sensors, my_fields)
+        response = ingest_data(request, Sensors, my_fields)
+        #if response.status_code == 200:
+            #stove_analysis()
+        requests.post("http://www.redoxygen.net/sms.dll?Action=SendSMS&AccountId=CI00206082&Email=annanm%40andrew%2Ecmu%2Eedu&Password=XddAv8r8&Recipient=4126166415&Message=Stove+Hot+Alert!!!")    
+        return response
     else:
         return handle_invalid_request(request)
     
-
+@csrf_exempt
+def stove_analysis():
+    try:
+        connection = psycopg2.connect(user='rpcs', password='rpcs2019', host='localhost', port='', database='rpcs')
+        cursor = connection.cursor()
+    except (Exception, psycopg2.Error) as error:
+        print('Error while fetching data from postgresql', error)
+    else:
+        query = "select * from hs_events order by timestamp desc limit 1"
+        cursor.execute(query)
+        new_stove_data = cursor.fetchone()
+        if "STOVE_HOT" in new_stove_data[4]:
+            requests.post("http://www.redoxygen.net/sms.dll?Action=SendSMS&AccountId=CI00206082&Email=annanm%40andrew%2Ecmu%2Eedu&Password=XddAv8r8&Recipient=4129236982&Message=StoveHot Alert!!!")
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
 
 
 
