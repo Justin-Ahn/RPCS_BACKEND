dff3816c4b21609eb0fae8cae2e888ce04d2e7da
justin-ahn <justinahn0@gmail.com>
2019-04-26 20:14:46 -0400

    And I hate underscores too Maybe I need more sleep

--- rpcs_db_server/ca/urls.py
+++ rpcs_db_server/ca/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('phys-incidents', views.phys_incidents),
     path('phys-model-params', views.phys_params),
     path('incident-summary', views.incident_summary),
-    path('sleep_trends', views.sleep_trends),
+    path('sleep-trends', views.sleep_trends),
 ]
