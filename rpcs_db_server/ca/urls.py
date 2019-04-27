f7514043a177d006dd05e6da3cb6f9bc933a2ac5
justin-ahn <justinahn0@gmail.com>
2019-04-26 20:11:36 -0400

    And support the new api & urls of course

--- rpcs_db_server/ca/urls.py
+++ rpcs_db_server/ca/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('phys-measures', views.phys_measures),
     path('phys-incidents', views.phys_incidents),
     path('phys-model-params', views.phys_params),
+    path('incident-summary', views.incident_summary)
+    path('sleep_trends', views.sleep_trends),
 ]
