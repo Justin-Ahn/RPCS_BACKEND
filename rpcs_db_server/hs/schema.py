d2b4dca350045e6005971a169ba0092eafd1c9b8
justin-ahn <justinahn0@gmail.com>
2019-04-27 14:49:07 -0400

    Notification functionality works + threading to reduce latency for response back for data ingestion

--- rpcs_db_server/hs/schema.py
+++ rpcs_db_server/hs/schema.py
@@ -1,12 +1,16 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .models import Events
+from .models import Events, Sensors
 from django.db.models import Q
 
 class EventsType(DjangoObjectType):
-	class Meta:
-		model = Events
+    class Meta:
+        model = Events
+
+class SensorsType(DjangoObjectType):
+    class Meta:
+        model = Sensors
 
 class CreateEvents(graphene.Mutation):
     event_type = graphene.String()
@@ -24,7 +28,8 @@ class CreateEvents(graphene.Mutation):
         data = graphene.String()
         event_id = graphene.Int()
 
-    def mutate(self, info, event_type, sensor_id, sensor_type, timestamp, data, event_id):
+    def mutate(self, info, event_type, sensor_id, sensor_type, timestamp, data, 
+        event_id):
         user = info.context.user or None
 
         events = Events(event_type=event_type, sensor_id=sensor_id, 
@@ -39,17 +44,62 @@ class CreateEvents(graphene.Mutation):
             data = events.data,
             event_id = events.event_id)
 
-class Query(graphene.ObjectType):
-	events = graphene.List(EventsType, id = graphene.Int())
-	
-	def resolve_events(self, info, id=-1, **kwargs):
-		if id>=0:
-			filter = (
-				Q(event_id=id)
-			)
-			return Events.objects.filter(filter)
+class CreateSensors(graphene.Mutation):
+    location = graphene.String()
+    sensor_type = graphene.String()
+    patient_id = graphene.Int()
+    sensor_id = graphene.ID()
 
-		return Events.objects.all()
+    class Arguments:
+        location = graphene.String()
+        sensor_type = graphene.String()
+        patient_id = graphene.Int()
+        sensor_id = graphene.ID()
+
+    def mutate(self, info, location, sensor_type, patient_id, sensor_id):
+        user = info.context.user or None
+
+        sensors = Sensors(location=location, sensor_type=sensor_type,
+            patient_id=patient_id, sensor_id=sensor_id)
+        sensors.save()
+
+        return CreateSensors(
+            location = sensors.location,
+            sensor_type = sensors.sensor_type,
+            patient_id = sensors.patient_id,
+            sensor_id = sensors.sensor_id)
+
+class Query(graphene.ObjectType):
+    events = graphene.List(EventsType, id = graphene.Int(),
+        filter_date = graphene.Boolean(),
+        start_date = graphene.types.datetime.DateTime(),
+        end_date = graphene.types.datetime.DateTime())
+
+    sensors = graphene.List(SensorsType, id = graphene.Int())
+    
+    def resolve_events(self, info, id=-1, filter_date=False, start_date=None, 
+        end_date=None, **kwargs):
+        if id >= 0:
+            if filter_date:
+                filter = (
+                    Q(event_id=id) & Q(timestamp__range=(start_date,end_date))
+                )
+            else:
+                filter = (
+                    Q(event_id=id)
+                )
+            return Events.objects.filter(filter)
+
+        return Events.objects.all()
+
+    def resolve_sensors(self, info, id=-1, **kwargs):
+        if id >= 0:
+            filter = (
+                Q(patient_id=id)
+            )
+            return Sensors.objects.filter(filter)
+        return Sensors.objects.all()
 
 class Mutation(graphene.ObjectType):
-	create_events = CreateEvents.Field()
+    create_events = CreateEvents.Field()
+    create_sensors = CreateSensors.Field()
--- rpcs_db_server/hs/views.py
+++ rpcs_db_server/hs/views.py
@@ -3,8 +3,10 @@ from django.views.decorators.csrf import csrf_exempt
 from hs.models import Events, Sensors
 from rpcs_db_server.utils import authorized, ingest_data, return_data, handle_invalid_request, \
     json_timestamp_customizer, json_j2str_customizer
+from hs.ca import hs_analysis 
 import psycopg2
 import requests
+import threading
 
 # Create your views here.
 
@@ -23,7 +25,10 @@ def events(request):
     if request.method == "GET":
         return return_data(request, Events, filterable_params)
     elif request.method == "POST":
-        return ingest_data(request, Events, my_fields, json_customizer=hs_customizer)
+        response = ingest_data(request, Events, my_fields, json_customizer=hs_customizer)
+        if response.status_code == 200:
+            threading.Thread(target=stove_analysis).start()
+        return response
     else:
         return handle_invalid_request(request)
 
@@ -37,11 +42,7 @@ def sensors(request):
     if request.method == "GET":
         return return_data(request, Sensors, filterable_params)
     elif request.method == "POST":
-        response = ingest_data(request, Sensors, my_fields)
-        #if response.status_code == 200:
-            #stove_analysis()
-        requests.post("http://www.redoxygen.net/sms.dll?Action=SendSMS&AccountId=CI00206082&Email=annanm%40andrew%2Ecmu%2Eedu&Password=XddAv8r8&Recipient=4126166415&Message=Stove+Hot+Alert!!!")    
-        return response
+        return ingest_data(request, Sensors, my_fields)
     else:
         return handle_invalid_request(request)
     
@@ -57,7 +58,8 @@ def stove_analysis():
         cursor.execute(query)
         new_stove_data = cursor.fetchone()
         if "STOVE_HOT" in new_stove_data[4]:
-            requests.post("http://www.redoxygen.net/sms.dll?Action=SendSMS&AccountId=CI00206082&Email=annanm%40andrew%2Ecmu%2Eedu&Password=XddAv8r8&Recipient=4129236982&Message=StoveHot Alert!!!")
+            result =requests.post("http://www.redoxygen.net/sms.dll?Action=SendSMS&AccountId=CI00206082&Email=annanm%40andrew%2Ecmu%2Eedu&Password=XddAv8r8&Recipient=4126166415&Message=Stove+Hot+Alert!!!")
+            hs_analysis(connection, cursor)
     finally:
         if connection:
             cursor.close()
